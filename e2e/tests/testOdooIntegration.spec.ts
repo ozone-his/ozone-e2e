3623a706eef7765fd9216e9e4b0e9c9edd9b0433
kdaud <dkakumirizii@gmail.com>
2023-06-12 14:51:17 +0300

    OZ-199: Random OpenMRS patient synced to Odoo and SENAITE

--- e2e/tests/testOdooIntegration.spec.ts
+++ e2e/tests/testOdooIntegration.spec.ts
@@ -1,7 +1,9 @@
 import { test, expect } from '@playwright/test';
 import { HomePage } from '../utils/functions/testBase';
+import { patientName } from '../utils/functions/testBase';
 
 let homePage: HomePage;
+let patientFullName = patientName.firstName + ' ' + patientName.givenName;
 
 test.beforeEach(async ({ page }) =>  {
     const homePage = new HomePage(page);
@@ -14,18 +16,18 @@ test.beforeEach(async ({ page }) =>  {
     await homePage.goToOdoo();
 });
 
-test('patient with lab order becomes customer in Odoo ', async ({ page }) => {
+test('patient with lab order becomes customer in Odoo', async ({ page }) => {
   await page.locator("//a[contains(@class, 'full')]").click();
   await page.getByRole('menuitem', { name: 'Sales' }).click();
   await page.getByPlaceholder('Search...').click();
-  await page.getByPlaceholder('Search...').type('Mikeal Edwards');
+  await page.getByPlaceholder('Search...').type(`${patientFullName}`);
   await page.getByPlaceholder('Search...').press('Enter');
   await page.waitForSelector("div.table-responsive table thead tr th:nth-child(4)");
 
   // syncs patient as an Odoo customer
   const customer =
   await page.locator("table tbody tr:nth-child(1) td.o_data_cell.o_field_cell.o_list_many2one.o_readonly_modifier.o_required_modifier").textContent();
-  await expect(customer?.includes("Mikeal Edwards")).toBeTruthy();
+  await expect(customer?.includes(`${patientFullName}`)).toBeTruthy();
 
   // amends customer running quotation
   const quotation =
--- e2e/tests/testSenaiteIntegration.spec.ts
+++ e2e/tests/testSenaiteIntegration.spec.ts
@@ -1,7 +1,9 @@
 import { test, expect } from '@playwright/test';
 import { HomePage } from '../utils/functions/testBase';
+import { patientName } from '../utils/functions/testBase';
 
 let homePage: HomePage;
+let patientFullName = patientName.firstName + ' ' + patientName.givenName;
 
 test.beforeEach(async ({ page }) =>  {
     const homePage = new HomePage(page);
@@ -16,21 +18,16 @@ test.beforeEach(async ({ page }) =>  {
     await expect(page).toHaveURL(/.*senaite/);
 });
 
-test('patient with lab order becomes client in SENAITE ', async ({ page }) => {
+test('patient with lab order becomes client in SENAITE', async ({ page }) => {
   await page.locator("//i[contains(@class, 'sidebar-toggle-icon')]").click();
   await page.getByRole('link', { name: 'Samples Samples' }).click();
-  await page.getByRole('textbox', { name: 'Search' }).type('Mikeal Edwards');
-  await page.locator('div.col-sm-3.text-right button:nth-child(1) i').click();
+  await page.getByRole('textbox', { name: 'Search' }).type(`${patientFullName}`);
   await page.locator('div.col-sm-3.text-right button:nth-child(2) i').click();
 
   // syncs patient as a SENAITE client
   const client =
   await page.locator('table tbody tr:nth-child(1) td.contentcell.Client div span span a').textContent();
-  await expect(client?.includes("Mikeal Edwards")).toBeTruthy();
-
-  const status =
-  await page.locator('table tbody tr:nth-child(1) td.contentcell.state_title div span span').textContent();
-  await expect(status?.includes("Sample due")).toBeTruthy();
+  await expect(client?.includes(`${patientFullName}`)).toBeTruthy();
 });
 
 test.afterEach(async ( {page}) =>  {
