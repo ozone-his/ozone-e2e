36bed1283110446ffb5f434ccc3e1088cccd1fc7
kdaud <dkakumirizii@gmail.com>
2023-06-20 12:42:33 +0300

    Add a test for debugging purpose

--- e2e/utils/functions/testBase.ts
+++ e2e/utils/functions/testBase.ts
@@ -145,6 +145,9 @@ export class HomePage {
     await this.page.getByLabel('Prescription refills').fill('3');
     await this.page.getByPlaceholder('e.g. "Hypertension"').type('Hypertension');
     await this.page.getByRole('button', { name: 'Save order' }).click({ force: true });
+
+    await expect(this.page.getByText('ERROR')).toBeVisible();
+
     await this.page.getByRole('button', { name: 'Sign and close' }).click({ force: true });
     await delay(5000);
   }
