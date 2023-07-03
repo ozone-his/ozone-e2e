9f405291bc649499dfda08bb9aae4b706a80c4d2
Daud Kakumirizi <dkakumirizii@gmail.com>
2023-07-03 11:12:04 +0300

    OZ-235: Revising a synced drug order edits corresponding quotation line in Odoo (#13)

--- e2e/tests/testOdooIntegration.spec.ts
+++ e2e/tests/testOdooIntegration.spec.ts
@@ -47,6 +47,35 @@ test('patient with drug order becomes customer in Odoo', async ({ page }) => {
   await expect(quotation?.includes("Quotation")).toBeTruthy();
 });
 
+test('Revising a synced drug order edits corresponding quotation line in Odoo', async ({ page }) => {
+  // setup
+  const homePage = new HomePage(page);
+  await homePage.createDrugOrder();
+  await homePage.goToOdoo();
+  await homePage.searchCustomerInOdoo();
+
+  const customer =
+  await page.locator("table tbody tr:nth-child(1) td.o_data_cell.o_field_cell.o_list_many2one.o_readonly_modifier.o_required_modifier").textContent();
+  await expect(customer?.includes(`${patientName.firstName + ' ' + patientName.givenName}`)).toBeTruthy();
+  await page.getByRole('cell', { name: `${patientName.firstName + ' ' + patientName.givenName}` }).click();
+  const drugOrderItem =   await page.locator("table tbody tr:nth-child(1) td.o_data_cell.o_field_cell.o_list_text.o_section_and_note_text_cell.o_required_modifier span");
+
+  await expect(drugOrderItem).toContainText('4.0 Tablet');
+  await expect(drugOrderItem).toContainText('Twice daily - 5 Days');
+
+  // replay
+  await page.goto('https://ozone-qa.mekomsolutions.net/openmrs/spa/home');
+  await homePage.searchPatient(`${patientName.firstName + ' ' + patientName.givenName}`);
+  await homePage.editDrugOrder();
+
+  // verify
+  await page.goto('https://erp.ozone-qa.mekomsolutions.net/web');
+  await homePage.searchCustomerInOdoo();
+  await page.getByRole('cell', { name: `${patientName.firstName + ' ' + patientName.givenName}` }).click();
+  await expect(drugOrderItem).toContainText('8.0 Tablet');
+  await expect(drugOrderItem).toContainText('Thrice daily - 6 Days');
+});
+
 test.afterEach(async ( {page}) =>  {
   const homePage = new HomePage(page);
   await homePage.deletePatient();
