b41f56d870fb08c718bc6c410b46de095ad0fcc4
Daud Kakumirizi <dkakumirizii@gmail.com>
2023-07-10 11:48:21 +0300

    OZ-249: Editing patient details with a synced lab order edits the corresponding customer details in Odoo (#20)
    Co-authored-by: Ruhanga <41738040+Ruhanga@users.noreply.github.com>

--- e2e/tests/testOdooIntegration.spec.ts
+++ e2e/tests/testOdooIntegration.spec.ts
@@ -32,6 +32,36 @@ test('Patient with lab order becomes customer in Odoo', async ({ page }) => {
   await expect(quotation?.includes("Quotation")).toBeTruthy();
 });
 
+test('Editing patient details with a synced lab order edits the corresponding customer details in Odoo', async ({ page }) => {
+  // setup
+  const homePage = new HomePage(page);
+  await homePage.createLabOrder();
+  await homePage.goToOdoo();
+  await homePage.searchCustomerInOdoo();
+  const customer =
+  await page.locator("table tbody tr:nth-child(1) td.o_data_cell.o_field_cell.o_list_many2one.o_readonly_modifier.o_required_modifier").textContent();
+  await expect(customer?.includes(`${patientName.firstName + ' ' + patientName.givenName}`)).toBeTruthy();
+
+  const quotation =
+  await page.locator("table tbody tr:nth-child(1) td.o_data_cell.o_field_cell.o_badge_cell.o_readonly_modifier span").textContent();
+  await expect(quotation?.includes("Quotation")).toBeTruthy();
+
+  // replay
+  await page.goto(`${process.env.E2E_BASE_URL}/openmrs/spa/home`);
+  await homePage.searchPatient(`${patientName.firstName + ' ' + patientName.givenName}`);
+  await homePage.updatePatientDetails();
+
+  // verify
+  await page.goto("https://erp.ozone-qa.mekomsolutions.net/web");
+  await homePage.searchUpdatedCustomerInOdoo();
+  const updatedCustomer =
+  await page.locator("table tbody tr:nth-child(1) td.o_data_cell.o_field_cell.o_list_many2one.o_readonly_modifier.o_required_modifier");
+
+  await expect(updatedCustomer).toHaveText('Winniefred' + ' ' + `${patientName.givenName }`);
+  await expect(quotation?.includes("Quotation")).toBeTruthy();
+  patientName.firstName = 'Winniefred';
+});
+
 test('Patient with drug order becomes customer in Odoo', async ({ page }) => {
   // setup
   const homePage = new HomePage(page);
