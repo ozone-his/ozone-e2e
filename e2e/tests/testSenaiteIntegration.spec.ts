a8a65daf57947139017222ec10d42c99f810b5d8
Daud Kakumirizi <dkakumirizii@gmail.com>
2023-07-13 16:23:21 +0300

    OZ-236: Editing patient details with a synced lab test order edits client details in SENAITE (#23)

--- e2e/tests/testSenaiteIntegration.spec.ts
+++ e2e/tests/testSenaiteIntegration.spec.ts
@@ -24,13 +24,30 @@ test(' Patient with lab order becomes client with analysis request in SENAITE', 
   await homePage.searchClientInSENAITE();
 
   // verify
-  const client =
-  await page.locator('table tbody tr:nth-child(1) td.contentcell.Client div span span a').textContent();
-  await expect(client?.includes(`${patientName.firstName + ' ' + patientName.givenName}`)).toBeTruthy();
+  const client = await page.locator('table tbody tr:nth-child(1) td.contentcell.title div span a');
+  await expect(client).toContainText(`${patientName.firstName + ' ' + patientName.givenName}`);
+});
 
-  const status =
-  await page.locator('table tbody tr:nth-child(1) td.contentcell.state_title div span span').textContent();
-  await expect(status?.includes("Sample due")).toBeTruthy();
+test('Editing patient details with a synced lab test order edits client details in SENAITE', async ({ page }) => {
+  // setup
+  const homePage = new HomePage(page);
+  await homePage.createLabOrder();
+  await homePage.goToSENAITE();
+  await homePage.searchClientInSENAITE();
+  const client = await page.locator('table tbody tr:nth-child(1) td.contentcell.title div span a');
+  await expect(client).toContainText(`${patientName.firstName + ' ' + patientName.givenName}`);
+
+  // replay
+  await page.goto(`${process.env.E2E_BASE_URL}/openmrs/spa/home`);
+  await homePage.searchPatient(`${patientName.firstName + ' ' + patientName.givenName}`);
+  await homePage.updatePatientDetails();
+
+  // verify
+  await homePage.goToSENAITE();
+  await homePage.searchUpdatedClientInSENAITE();
+
+  await expect(client).toContainText('Winniefred' + ' ' + `${patientName.givenName }`);
+  patientName.firstName = 'Winniefred';
 });
 
 test.afterEach(async ( {page}) =>  {
