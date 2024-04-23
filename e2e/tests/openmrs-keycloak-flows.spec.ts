5ddbcc967467f0bd71151914cd4939b31c4f4fa3
Dimitri R <dimitri@mekomsolutions.com>
2024-04-23 16:18:44 +0200

    Update openmrs-keycloak-flows.spec.ts

--- e2e/tests/openmrs-keycloak-flows.spec.ts
+++ e2e/tests/openmrs-keycloak-flows.spec.ts
@@ -168,7 +168,7 @@ test('Deleting a synced Superset role deletes the corresponding Keycloak role.',
   await expect(page.getByText(`${randomSupersetRoleName.roleName}`)).not.toBeVisible();
 });
 
-test('Deleted synced Keycloak role gets recreated in the subsequent polling cycle.', async ({ page }) => {
+test('A role deleted from within Keycloak gets recreated in the subsequent polling cycle.', async ({ page }) => {
   // setup
   homePage = new HomePage(page);
   await homePage.goToSuperset();
@@ -192,7 +192,7 @@ test('Deleted synced Keycloak role gets recreated in the subsequent polling cycl
   await homePage.deleteSupersetRole();
 });
 
-test('Created (non-synced) Keycloak role gets deleted in the subsequent polling cycle.', async ({ page }) => {
+test('A role created from within Keycloak gets deleted in the subsequent polling cycle.', async ({ page }) => {
   // setup
   homePage = new HomePage(page);
   await homePage.goToKeycloak();
