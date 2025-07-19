f81fa07705f9f347b68ecd9fb4f59a4c50a8eed0
kdaud <dkakumirizii@gmail.com>
2025-07-19 19:55:25 +0300

    Test dev server

--- e2e/utils/functions/openmrs.ts
+++ e2e/utils/functions/openmrs.ts
@@ -404,6 +404,8 @@ export class OpenMRS {
     await this.page.getByLabel(/my account/i).click();
     await expect(this.page.getByRole('button', { name: /logout/i })).toBeVisible();
     await this.page.getByRole('button', { name: /logout/i }).click();
+    let keycloak = new Keycloak(this.page);
+    await keycloak.confirmLogout();
     await expect(this.page).toHaveURL(/.*login/);
   }
 }
