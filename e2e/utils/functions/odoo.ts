604988add5fdd30f16e92f86bf16c9b37c114829
Daud Kakumirizi <dkakumirizii@gmail.com>
2025-07-31 09:46:19 +0300

    OZ-949: Update e2e tests for Odoo flows to run on Odoo 17 (#142)

--- e2e/utils/functions/odoo.ts
+++ e2e/utils/functions/odoo.ts
@@ -13,11 +13,6 @@ export class Odoo {
 
   async open() {
     await this.page.goto(`${ODOO_URL}`);
-    await this.page.getByRole('link', { name: /login with single sign-on/i }).click(), delay(4000);
-    if(await this.page.locator('#username').isVisible()) {
-      const keycloak = new Keycloak(this.page);
-      await keycloak.enterCredentials();
-    }
     await expect(this.page).toHaveURL(/.*web/);
   }
 
@@ -37,6 +32,7 @@ export class Odoo {
   }
 
   async searchCustomer() {
+    await this.page.getByRole('button', { name: /remove/i }).click(), delay(2000);
     await expect(this.page.locator('.o_searchview_input')).toBeVisible();
     await this.page.locator('.o_searchview_input').fill(`${patientName.firstName + ' ' + patientName.givenName}`);
     await this.page.locator('.o_searchview_input').press('Enter');
@@ -44,26 +40,24 @@ export class Odoo {
   }
 
   async navigateToSales() {
-    await this.page.locator("//a[contains(@class, 'full')]").click();
-    await expect(this.page.getByRole('menuitem', { name: /sales/i })).toBeVisible();
-    await this.page.getByRole('menuitem', { name: /sales/i }).click();
-    await expect(this.page.locator('.breadcrumb-item')).toHaveText(/quotations/i);
+    await this.page.locator('button[title="Home Menu"]').click(), delay(1500);
+    await this.page.getByRole('menuitem', { name: /sales/i }).first().click();
+    await expect(this.page.locator('span:has-text("Quotations")')).toBeVisible();
   }
 
   async createSaleOrderLine() {
-    await this.page.getByRole('button', { name: /create/i }).click();
-    await expect(this.page.locator('li.breadcrumb-item:nth-child(2)')).toHaveText(/new/i);
+    await this.page.getByRole('button', { name: /new/i }).click();
     await this.page.getByLabel('Customer', { exact: true }).type(`${patientName.firstName + ' ' + patientName.givenName}`);
     await this.page.getByText(`${patientName.firstName + ' ' + patientName.givenName}`).first().click();
     await this.page.getByRole('button', { name: 'Add a product' }).click();
     await this.page.locator('td.o_data_cell:nth-child(2) div:nth-child(1) input').fill('Acétaminophene Co 500mg');
     await this.page.getByText('Acétaminophene Co 500mg').first().click();
-    await this.page.locator('input[name="product_uom_qty"]').fill('8');
-    await this.page.locator('td.o_data_cell:nth-child(7) input').fill('2.00');
-    await this.page.locator('td.o_data_cell:nth-child(9)').click(), delay(2000);
-    await expect(this.page.locator('td.o_data_cell:nth-child(9)')).toHaveText('$ 16.00');
+    await this.page.locator('input[inputmode="decimal"]').nth(0).fill('8');
+    await this.page.locator('input[inputmode="decimal"]').nth(1).fill('2.00'), delay(1500);
+    await this.page.locator('.o_readonly_modifier.text-muted').click(), delay(2000);
+    await expect(this.page.locator('.o_readonly_modifier.text-muted')).toHaveText('$ 16.00');
     await this.page.getByRole('button', { name: /confirm/i }).click(), delay(3000);
-    await expect(this.page.locator('td.o_data_cell:nth-child(2) span:nth-child(1) span')).toHaveText('Acétaminophene Co 500mg');
+    await expect(this.page.locator('td[name="product_template_id"] span')).toHaveText('Acétaminophene Co 500mg');
     await expect(this.page.locator('td.o_data_cell:nth-child(4)')).toHaveText('8');
     await expect(this.page.locator('td.o_data_cell:nth-child(9)')).toHaveText('2.00');
     await expect(this.page.locator('td.o_data_cell:nth-child(11)')).toHaveText('$ 16.00');
@@ -77,28 +71,28 @@ export class Odoo {
     await this.page.getByRole('button', { name: 'Add a product' }).click();
     await this.page.locator('td.o_data_cell:nth-child(2) div:nth-child(1) input').fill('Acyclovir Sirop 200mg');
     await this.page.getByText('Acyclovir Sirop 200mg').first().click();
-    await this.page.locator('input[name="product_uom_qty"]').fill('6');
-    await this.page.locator('td.o_data_cell:nth-child(7) input').fill('2.00');
-    await this.page.locator('td.o_data_cell:nth-child(9)').click(), delay(2000);
-    await expect(this.page.locator('td.o_data_cell:nth-child(9)')).toHaveText('$ 12.00');
+    await this.page.locator('input[inputmode="decimal"]').nth(0).fill('6');
+    await this.page.locator('input[inputmode="decimal"]').nth(1).fill('2.00');
+    await this.page.locator('.o_readonly_modifier.text-muted').click(), delay(2000);
+    await expect(this.page.locator('.o_readonly_modifier.text-muted')).toHaveText('$ 12.00');
     await this.page.getByRole('button', { name: /save/i }).click(), delay(3000);
-    await expect(this.page.locator('td.o_data_cell:nth-child(2) span:nth-child(1) span')).toHaveText('Acyclovir Sirop 200mg');
+    await expect(this.page.locator('td[name="product_template_id"] span')).toHaveText('Acyclovir Sirop 200mg');
   }
 
   async modifySaleOrderLine() {
     await this.page.getByRole('button', { name: /edit/i }).click();
     await this.page.getByText(/acétaminophene co 500mg/i).nth(1).click();
-    await this.page.locator('input[name="product_uom_qty"]').fill('10');
-    await this.page.locator('input[name="price_unit"]').fill('3');
-    await this.page.locator('td.o_field_x2many_list_row_add').click(), delay(2000);
-    await expect(this.page.locator('td.o_data_cell:nth-child(11)')).toHaveText('$ 30.00');
+    await this.page.locator('input[inputmode="decimal"]').nth(0).fill('10');
+    await this.page.locator('input[inputmode="decimal"]').nth(1).fill('3');
+    await this.page.locator('.o_readonly_modifier.text-muted').click(), delay(2000);
+    await expect(this.page.locator('.o_readonly_modifier.text-muted')).toHaveText('$ 30.00');
     await this.page.getByRole('button', { name: /save/i }).click(), delay(3000);
   }
 
   async voidSaleOrderLine() {
     await this.page.getByRole('button', { name: /edit/i }).click();
     await this.page.getByText(/acétaminophene co 500mg/i).nth(1).click();
-    await this.page.locator('input[name="product_uom_qty"]').fill('0');
+    await this.page.locator('input[inputmode="decimal"]').nth(0).fill('0');
     await this.page.getByRole('button', { name: /save/i }).click(), delay(1000);
     await this.page.getByRole('button', { name: 'Ok' }).click(), delay(3000);
   }
@@ -137,12 +131,12 @@ export class Odoo {
   }
 
   async createGroup() {
-    await this.page.getByRole('button', { name: /create/i }).click();
+    await this.page.getByRole('button', { name: /new/i }).click();
     await this.page.getByLabel(/application/i).click();
     await expect(this.page.getByText(/accounting/i)).toBeVisible();
     await this.page.getByText(/accounting/i).click();
     await this.page.getByLabel(/name/i).fill(`${randomOdooGroupName.groupName}`);
-    await this.page.getByRole('button', { name: /save/i }).click(), delay(250000);
+    await this.page.getByRole('button', { name: /save/i }).click(), delay(10000);
   }
 
   async searchUser() {
@@ -155,28 +149,24 @@ export class Odoo {
 
   async searchGroup() {
     await this.page.getByLabel(/remove/i).click();
-    await expect(this.page.locator('div>div>input')).toBeVisible();
-    await this.page.locator('div>div>input').type(`${randomOdooGroupName.groupName}`);
+    await expect(this.page.getByRole('searchbox', { name: /search/i })).toBeVisible();
+    await this.page.getByRole('searchbox', { name: /search/i }).type(`${randomOdooGroupName.groupName}`);
     await this.page.locator('div>div>input').press('Enter');
-    await this.page.getByRole('cell', { name: `${randomOdooGroupName.groupName}` }).click();
+    await this.page.getByRole('cell', { name: `${randomOdooGroupName.groupName}` }).click(), delay(4000)
   }
 
   async updateGroup() {
-    await expect(this.page.getByRole('button', { name: /edit/i })).toBeVisible();
-    await this.page.getByRole('button', { name: /edit/i }).click();
     await this.page.getByLabel(/name/i).fill(`${randomOdooGroupName.updatedGroupName}`);
     await this.page.getByRole('button', { name: /save/i }).click();
-    randomOdooGroupName.groupName = `${randomOdooGroupName.updatedGroupName}`, delay(250000);
+    randomOdooGroupName.groupName = `${randomOdooGroupName.updatedGroupName}`, delay(10000);
   }
 
   async deleteGroup() {
-    await expect(this.page.getByRole('button', { name: /action/i })).toBeVisible();
-    await this.page.getByRole('button', { name: /action/i }).click();
-    await expect(this.page.getByRole('menuitemcheckbox', { name: /delete/i })).toBeVisible();
-    await this.page.getByRole('menuitemcheckbox', { name: /delete/i }).click();
-    await expect(this.page.getByRole('button', { name: /ok/i })).toBeVisible();
-    await this.page.getByRole('button', { name: /ok/i }).click(), delay(2000);
-    await expect(this.page.getByText(`${randomOdooGroupName.groupName}` )).not.toBeVisible(), delay(240000);
+    const actionsButton = this.page.locator('button:has(i[data-tooltip="Actions"])');
+    await actionsButton.waitFor({ state: 'visible' });
+    await actionsButton.click();
+    await this.page.getByRole('menuitem', { name: /delete/i }).click(), delay(1500);
+    await this.page.getByRole('button', { name: /delete/i }).click(), delay(10000);
   }
 
   async logout() {
